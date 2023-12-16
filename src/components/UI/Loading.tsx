2cbc2465b192449c7b2332533be187097a0aaa53
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-12-16 22:36:17 +0330

    update loading

--- src/components/UI/Loading.tsx
+++ src/components/UI/Loading.tsx
@@ -2,9 +2,11 @@ import styles from "./loading.module.css";
 
 const Loading = () => {
   return (
-    <div className={styles.loading}>
-      <div></div>
-      <div></div>
+    <div className={styles.parent}>
+      <div className={styles.loading}>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
